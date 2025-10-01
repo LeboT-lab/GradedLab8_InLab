8b753d1feb072695cff1cd184b57a2b02cf1874c
LeboT-lab <sambolebo6@gmail.com>
2025-10-01 13:27:02 +0200

    Committed firebaseConfig

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAVzuYq_Kx5eB23UJGwNcE6jG2CUTrzABo",
+  authDomain: "tasks-cd6d9.firebaseapp.com",
+  projectId: "tasks-cd6d9",
+  storageBucket: "tasks-cd6d9.firebasestorage.app",
+  messagingSenderId: "173211617709",
+  appId: "1:173211617709:web:64afc1c9de44c2187ab924"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
